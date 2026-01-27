4adb1ac9a077dde5ad3bf6fd180612d74d9196cb
johan <johan.persson@iths.se>
2026-01-27 11:59:14 +0100

    uppdaterar projektet

--- src/main/java/se/iths/johan/gitlabb/controller/HomeController.java
+++ src/main/java/se/iths/johan/gitlabb/controller/HomeController.java
@@ -5,16 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
     @GetMapping
-    public @ResponseBody String home(Model model) {
-        model.addAttribute("title", "Home");
-        return "Välkommen!";
+    public String home(Model model) {
+        model.addAttribute("greeting", "Välkommen");
+        return "home";
     }
 }
 
