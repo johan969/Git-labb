97f5bebbce5728aff27e8dde3b644c3346dc6193
Joel-java-jpg <Joel.schou@iths.se>
2026-01-28 10:38:37 +0100

    uppdaterat

--- src/main/java/se/iths/johan/gitlabb/controller/HomeController.java
+++ src/main/java/se/iths/johan/gitlabb/controller/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
 
     @GetMapping
     public String home(Model model) {
-        model.addAttribute("greeting", "Välkommen");
+        model.addAttribute("greeting", "Välkommen Joel");
         return "home";
     }
 }
